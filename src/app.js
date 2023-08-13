0333dae219238793887c222f35768d62ef57e97e
heredianicolas <herediarobertonicolas@gmail.com>
2023-08-13 19:53:58 -0300

    cambiando configuracion

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const path = require("path");
 
 app.use(express.static("public"));
 
-app.listen(3000, ()=> console.log("puerto 3000 escuchando"))
+const port = process.env.PORT || 3001;
+app.listen(port, ()=> console.log("puerto 3000 escuchando"))
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"));
